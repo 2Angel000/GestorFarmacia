db10b59fe93d4db8ae8853bd4052c04f89d6b674
2Angel000 <mscmorfy_arcangel13@hotmail.com>
2023-11-10 01:14:20 -0600

    Primer commit de aplicación en Java

---
+++ src/clases/Componentes.java
@@ -0,0 +1,96 @@
+package clases;
+
+public class Componentes {
+    public static String acercaDe = "<html>"
+            + "<p style='text-align:justify;'>"
+            + "Bienvenido a <strong>" + Globales.nombreFarmacia + "</strong>, un programa gestor de farmacia diseñado para ayudar a administrar eficazmente su farmacia y mejorar la atención al cliente."
+            + " Somos un equipo dedicado a proporcionar soluciones de software innovadoras."
+            + "</p><p></p>"
+            + "<p style='text-align:justify;'>"
+            + "Nuestra misión es brindar a los propietarios de farmacias y a su personal las herramientas necesarias para llevar un control más preciso de inventario, gestionar recetas y proporcionar un servicio excepcional a los pacientes."
+            + "</p><p></p>"
+            + "<p style='text-align:justify;'>Con <strong>" + Globales.nombreFarmacia + "</strong>, puedes:</p>"
+            + "<ul>"
+            + "<li>Administrar inventario de productos farmacéuticos.</li>"
+            + "<li>Registrar y gestionar recetas médicas.</li>"
+            + "<li>Facilitar la facturación y el seguimiento de ventas.</li>"
+            + "<li>Ofrecer a tus clientes un servicio más rápido y personalizado.</li>"
+            + "</ul>"
+            + "</html>";
+
+    public  static String contacto = "<html>\n"
+            + "<head>\n"
+            + "    <style>\n"
+            + "        p {\n"
+            + "            text-align: justify;\n"
+            + "        }\n"
+            + "        a {\n"
+            + "            text-decoration: none;\n"
+            + "            color: blue;\n"
+            + "        }\n.titulo{font-size='20px';text-align:center;}"
+            + "    </style>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "    <p class='titulo'>\n"
+            + "        .<strong>Hola, ¡Bienvenido a Contacto de programador.</strong></p><br><br>\n"
+            + "        <p>Número de Teléfono: 747 152 3096<br>\n"
+            + "        Número de Teléfono 2: 747 153 6547<br>\n"
+            + "        Correo: <a href=\"mailto:miguelangelfloma13@gmail.com\">miguelangelfloma13@gmail.com</a><br>\n"
+            + "        GitHub: <a href=\"https://github.com/2Angel000?tab=repositories\">https://github.com/2Angel000?tab=repositories</a><br>\n"
+            + "        LinkedIn: <a href=\"https://www.linkedin.com/in/mangel13/\">https://www.linkedin.com/in/mangel13/</a><br><br>\n"
+            + "        ¡Saludos!\n"
+            + "    </p>\n"
+            + "</body>\n"
+            + "</html>";
+
+    public  static String copy = "\u00A9 2023 "+Globales.nombreFarmacia;
+
+    public  static String dash = "Dashboard "+Globales.nombreFarmacia;
+
+    public static String tablero = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 18px;'>TABLERO</span></div></html>";
+    public static String productos = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>GESTIONAR</span><br>\n" +
+            "<span style='font-size: 18px;'>PRODUCTOS</span></div></html>";
+    
+    public static String inventario = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>CONSULTAR</span><br>\n" +
+            "<span style='font-size: 18px;'>INVENTARIO</span></div></html>";
+    
+    public static String caja = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>CONSULTAR</span><br>\n" +
+            "<span style='font-size: 18px;'>CAJA</span></div></html>";
+    
+    public static String facturas = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>GESTIONAR</span><br>\n" +
+            "<span style='font-size: 18px;'>FACTURAS</span></div></html>";
+    
+    public static String proveedores = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>GESTIONAR</span><br>\n" +
+            "<span style='font-size: 18px;'>PROVEEDORES</span></div></html>";
+    
+    public static String empleados = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>GESTIONAR</span><br>\n" +
+            "<span style='font-size: 18px;'>EMPLEADOS</span></div></html>";
+    
+    public static String ventas = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>CONSULTAR</span><br>\n" +
+            "<span style='font-size: 18px;'>VENTAS</span></div></html>";
+    
+    public static String reportes = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>CONSULTAR</span><br>\n" +
+            "<span style='font-size: 18px;'>REPORTES</span></div></html>";
+    
+    public static String recordatorios = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>CONSULTAR</span><br>\n" +
+            "<span style='font-size: 18px;'>RECORDATORIOS</span></div></html>";
+    
+    public static String usuario = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>CONFIGURACIÓN DE</span><br>\n" +
+            "<span style='font-size: 18px;'>USUARIO</span></div></html>";
+    public static String compra = "<html><div style='text-align: center;'>\n" +
+            "<span style='font-size: 14px;'>REALIZAR</span><br>\n" +
+            "<span style='font-size: 18px;'>COMPRA</span></div></html>";
+
+
+}
---
+++ src/clases/Globales.java
@@ -0,0 +1,6 @@
+package clases;
+
+public class Globales {
+    public static String  nombreFarmacia = "FARMACIA NUEVA VIDA";
+    public static String dashboardTitle = "Dashboard - "+nombreFarmacia;
+}
